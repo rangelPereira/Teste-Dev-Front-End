70aa9fbdd66a7ddebfe226984e3cf4e594d561be
Rangel Pereira <dev@rangelpereira.com.br>
2020-09-24 09:51:58 -0300

    iniciando projeto

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return(
+      <Container>
+          <h1>hellow Word</h1>
+      </Container>
+  );
+}
+
+export default Layout;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,39 @@
+import {createGlobalStyle} from 'styled-components';
+import './Fontes/Nunito-Regular.ttf';
+
+
+export default createGlobalStyle`
+  *{
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+
+    color: var(--white);
+  }
+
+  html, body, #root{
+      max-width: 100vw;
+      max-height: 100vh;
+
+      width: 100%;
+      height: 100%;
+  }
+
+  *, button, input{
+      border: 0;
+      background: none;
+      font-family: 'Nunito', sans-serif;
+  }
+  *:hover{
+    transition: all 0.2s;
+  }
+
+  html{
+      background: var(--primary);
+  }
+  :root {
+    --primary: #fff;
+    --secondary: #000;
+    --flag: #F8475F;
+  }
+`;
